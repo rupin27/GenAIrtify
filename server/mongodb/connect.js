c50191670676f31a9c4f251dcdbaeeff568358fc
rupin27 <rupinmehra@umass.edu>
2023-06-14 03:49:36 -0400

    Establishing connection with MongoDb database

---
+++ server/mongodb/connect.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDB = (url) => {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(url)
+      .then(() => console.log('connected to mongo'))
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+export default connectDB
