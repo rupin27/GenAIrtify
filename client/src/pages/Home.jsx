d7359c2396a3aaca3f0826e27ae11b4b1ac97a91
rupin27 <rupinmehra@umass.edu>
2023-06-15 04:07:04 -0400

    update changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://genairtify.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
